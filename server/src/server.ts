49f5747adb479402a6030f328a08c49134124627
aimeedyki <akuwudike.a@gmail.com>
2018-04-18 15:46:28 +0100

    ch(code-climate): setup code climate
    [Maintains #156884873]

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
-import * as bodyParser from "body-parser";
-import * as cookieParser from "cookie-parser";
-import * as express from "express";
-import * as logger from "morgan";
-import * as path from "path";
-import errorHandler = require("errorhandler");
-import methodOverride = require("method-override");
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as path from 'path';
+
+import errorHandler = require('errorhandler');
+import methodOverride = require('method-override');
 
 import serverRoutes from './routes/index';
 
@@ -36,16 +37,16 @@ export class Server {
    * @constructor
    */
   constructor() {
-    //create expressjs application
+    // create expressjs application
     this.app = express();
 
-    //configure application
+    // configure application
     this.config();
 
-    //add routes
+    // add routes
     this.routes();
 
-    //add api
+    // add api
     this.api();
   }
 
@@ -56,7 +57,7 @@ export class Server {
    * @method api
    */
   public api() {
-    //empty for now
+    // empty for now
   }
 
   /**
@@ -66,37 +67,37 @@ export class Server {
    * @method config
    */
   public config() {
-    //add static paths
-    this.app.use(express.static(path.join(__dirname, "public")));
+    // add static paths
+    this.app.use(express.static(path.join(__dirname, 'public')));
 
-    //configure pug
-    // this.app.set("views", path.join(__dirname, "views"));
-    // this.app.set("view engine", "pug");
+    // configure pug
+    // this.app.set('views', path.join(__dirname, 'views'));
+    // this.app.set('view engine', 'pug');
 
-    //use logger middlware
-    this.app.use(logger("dev"));
+    // use logger middlware
+    this.app.use(logger('dev'));
 
-    //use json form parser middlware
+    // use json form parser middlware
     this.app.use(bodyParser.json());
 
-    //use query string parser middlware
+    // use query string parser middlware
     this.app.use(bodyParser.urlencoded({
       extended: true
     }));
 
-    //use cookie parser middleware
-    this.app.use(cookieParser("SECRET_GOES_HERE"));
+    // use cookie parser middleware
+    this.app.use(cookieParser('SECRET_GOES_HERE'));
 
-    //use override middlware
+    // use override middlware
     this.app.use(methodOverride());
 
-    //catch 404 and forward to error handler
-    this.app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+    // catch 404 and forward to error handler
+    this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
       err.status = 404;
       next(err);
     });
 
-    //error handling
+    // error handling
     this.app.use(errorHandler());
   }
 
